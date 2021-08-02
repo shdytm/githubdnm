a073dfb427d440bfdb2e3c947af89db34d5b2557
shdytm <suhedayetim18@gmail.com>
2021-08-02 22:59:06 +0200

    new added

--- src/deneme/d1.java
+++ src/deneme/d1.java
@@ -4,6 +4,7 @@ public class d1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi");
+        System.out.println("new");
 
     }
 }
