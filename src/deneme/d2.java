d86276940af92dbd33467b38d85283cbe0c14d7c
shdytm <suhedayetim18@gmail.com>
2021-08-07 21:16:52 +0200

    d2

---
+++ src/deneme/d2.java
@@ -0,0 +1,12 @@
+package deneme;
+
+public class d2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int sum = x + 2;
+        System.out.println(sum);
+
+        int y = 0;
+    }
+
+}
