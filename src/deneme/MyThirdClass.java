cbc681004d27c55f5fe3454318e40503d6350bef
shdytm <suhedayetim18@gmail.com>
2021-08-08 13:40:10 +0200

    files

--- src/deneme/MyThirdClass.java
+++ src/deneme/MyThirdClass.java
@@ -1,4 +1,19 @@
 package deneme;
 
 public class MyThirdClass {
+    public static void main(String[] args) {
+        int x=5;
+        int sum =x+2;
+        System.out.println(sum);
+
+        int y=0;
+
+    }
+
+
+
+
+
+
 }
+
