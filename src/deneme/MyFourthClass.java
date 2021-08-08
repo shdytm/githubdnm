753520f1bdaae50437e5ff7f1778b884309cce2a
shdytm <suhedayetim18@gmail.com>
2021-08-08 13:53:11 +0200

    fourth class

--- src/deneme/MyFourthClass.java
+++ src/deneme/MyFourthClass.java
@@ -1,20 +1,11 @@
 package deneme;
 
-public class MyThirdClass {
+public class MyFourthClass {
     public static void main(String[] args) {
-        int x=5;
-        int sum =x+2;
+        int x=543;
+        int sum=x+24;
         System.out.println(sum);
-
-        int y=0;
-
+        int y=50;
 
     }
-
-
-
-
-
-
 }
-
