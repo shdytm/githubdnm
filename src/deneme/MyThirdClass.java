4e6d76632245ebf44590c9c431b8785e2a9cb3b4
shdytm <88331390+shdytm@users.noreply.github.com>
2021-08-08 14:06:16 +0200

    Update MyThirdClass.java

--- src/deneme/MyThirdClass.java
+++ src/deneme/MyThirdClass.java
@@ -1,13 +1,13 @@
 package deneme;
-
 public class MyThirdClass {
     public static void main(String[] args) {
         int x=5;
         int sum =x+2;
         System.out.println(sum);
+        int y=0;
         int z=10;
 
-        int y=0;
+        
         System.out.println("THIS IS REMOTE REPO CODE");
 
 
