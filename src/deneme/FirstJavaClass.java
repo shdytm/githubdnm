a3e53405ba18358898ea4b9ff42002dfe0c7448b
shdytm <suhedayetim18@gmail.com>
2021-08-08 13:42:10 +0200

    delete first and second inhalt

--- src/deneme/FirstJavaClass.java
+++ src/deneme/FirstJavaClass.java
@@ -2,11 +2,7 @@ package deneme;
 
 public class FirstJavaClass {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hi");
-        System.out.println("new");
 
-        String str ="Java";
 
 
     }
--- src/deneme/SecondJavaClass.java
+++ src/deneme/SecondJavaClass.java
@@ -2,23 +2,7 @@ package deneme;
 
 public class SecondJavaClass {
     public static void main(String[] args) {
-        int x = 5;
-        int sum = x + 2;
-        System.out.println(sum);
 
-        int y = 0;
-
-        String str="yitti gari";
-
-        int eq=10;
-
-        System.out.println("Project");
-
-        String str1="school";
-
-
-        
-        System.out.println("This is remote repo code");
         
     }
 
