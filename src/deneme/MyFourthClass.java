3945b76724d3ac1f8d641628dd5cf5c5c619a31f
shdytm <suhedayetim18@gmail.com>
2021-08-08 14:07:29 +0200

    adding line 8

--- src/deneme/MyFourthClass.java
+++ src/deneme/MyFourthClass.java
@@ -1,10 +1,11 @@
 package deneme;
-
 public class MyFourthClass {
     public static void main(String[] args) {
         int x=543;
         int sum=x+24;
         System.out.println(sum);
+
+
         int y=50;
 
     }
--- src/deneme/MyThirdClass.java
+++ src/deneme/MyThirdClass.java
@@ -1,12 +1,12 @@
 package deneme;
-
 public class MyThirdClass {
     public static void main(String[] args) {
         int x=5;
         int sum =x+2;
         System.out.println(sum);
-
         int y=0;
+        String str="Java";
+
         System.out.println("THIS IS REMOTE REPO CODE");
 
 
